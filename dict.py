9aa3fa65068c2454cdf0a4dad0aaacbeffef3774
Yarisingu Tarun <137394306+yarasingu@users.noreply.github.com>
2023-10-03 12:40:09 +0000

    python

---
+++ dict.py
@@ -0,0 +1,4 @@
+person = {"first_name": "John", "last_name": "Doe", "age": 30}
+
+person_sub = {1:"java",2:"python"}
+
---
+++ variables.py
@@ -0,0 +1,29 @@
+name = "tarun"
+
+print(name[0:2])
+
+rename =str(name[0:2])
+print(rename)
+
+
+age = 10
+print(age)
+
+age = 10.0
+print(age)
+
+age = "ten"
+print(age)
+
+#complex variable
+
+complex = 2+4j
+print(2+4j)
+
+# Ordered collection of items in list and we cannot change 
+list = [1,2,3,4,5]
+print(list)
+
+# Unordered collection of unique items in set and  duplicates are automatically removed and we cannot change
+set = {1,2,3,4,5,5}
+print(set)
